f4f969c311e995b298dbb472991e85b452847c00
mta630 <michael.t.albert89@gmail.com>
2020-04-06 14:26:28 -0500

    Completed all tasks

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-// TODO
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map((item, index) =>
+    <GroceryListItem key={index} item={item}/>
+    )}
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hover: false
+    };
+
+  }
+
+  onListItemHover() {
+    this.setState({
+      hover : !this.state.hover
+    });
+  }
+
+  render() {
+    var style = {
+      cursor: 'pointer',
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+
+  	return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.item}</li>
+  	);
+  }
+
+}
+
+ReactDOM.render(<GroceryList items={['Milk', 'Eggs', 'Juice']}/>, document.getElementById('app'));
